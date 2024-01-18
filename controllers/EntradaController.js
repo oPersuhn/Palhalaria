9a20fe20e3c190b9b2cd06a24e23492bc174f5a6
oPersuhn <augusto.persuhn1@gmail.com>
2024-01-18 14:53:19 -0300

    adicionando o arquivo EntradaController

---
+++ controllers/EntradaController.js
@@ -0,0 +1,63 @@
+const db = require('../firebaseConfig');
+
+
+const EntradaController = {
+    createEntrada: async (req, res) => {
+        try {
+            const entradaRef = db.collection('entradas').doc();
+            await entradaRef.set(req.body);
+            res.status(201).json({ id: entradaRef.id, ...req.body });
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+
+    getAllEntradas: async (req, res) => {
+        try {
+            const entradasSnapshot = await db.collection('entradas').get();
+            const entradas = [];
+            entradasSnapshot.forEach(doc => {
+                entradas.push({ id: doc.id, ...doc.data() });
+            });
+            res.status(200).json(entradas);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+
+    getEntradaById: async (req, res) => {
+        try {
+            const entradaRef = db.collection('entradas').doc(req.params.id);
+            const doc = await entradaRef.get();
+            if (!doc.exists) {
+                res.status(404).send('Entrada não encontrada');
+            } else {
+                res.status(200).json({ id: doc.id, ...doc.data() });
+            }
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+
+    updateEntrada: async (req, res) => {
+        try {
+            const entradaRef = db.collection('entradas').doc(req.params.id);
+            await entradaRef.update(req.body);
+            res.status(200).send('Entrada atualizada com sucesso');
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+
+    deleteEntrada: async (req, res) => {
+        try {
+            const entradaRef = db.collection('entrada').doc(req.params.id);
+            await entradaRef.delete();
+            res.status(200).send('Entrada deletada com sucesso');
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    }
+};
+
+module.exports = EntradaController;
